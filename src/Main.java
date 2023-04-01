b31dfc2fe49734ec713521264ac9fbef8d203299
670lql <3196718529@qq.com>
2023-04-01 21:42:18 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Lex lex = new Lex("src/lex_analize/source.qi");
+        int a = 1;
     }
 }
